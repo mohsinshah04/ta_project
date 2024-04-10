87c6009811b2af056ac424c9f8eec15639b53a8a
emmet <ehbenck@uwm.edu>
2024-04-10 11:49:32 -0500

    Testing the commit and pull stuff

--- djangoProject1/ta_app/classes/UserAbstactClass.py
+++ djangoProject1/ta_app/classes/UserAbstactClass.py
@@ -2,4 +2,5 @@
 This class is an abstract class that is used to create all of the variations of a user that can be found on the app
 This is just a test
 This is just another test
+This yet again, another test
 """
