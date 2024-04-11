be2b07f57fe7b7e7eaeace73f30d0bcce7174a0a
emmet <ehbenck@uwm.edu>
2024-04-11 18:30:24 -0500

    Wrote what I thought the correct implementation is for the methods. Still struggling to get tests to work

--- djangoProject1/ta_app/classes/UserAbstractClass.py
+++ djangoProject1/ta_app/classes/UserAbstractClass.py
@@ -21,18 +21,46 @@ class UserAbstractClass:
         return toReturn
 
 
-
     def delete_user(self, user_ID):
-        pass
+        if self.role != "Supervisor":
+            return False
+        if user_ID is None:
+            return False
+        if User.objects.filter(User_ID=user_ID) is None:
+            return False
+        User.objects.delete(user_ID)
+        toReturn = not(User.objects.filter(User_ID=user_ID).exists())
+        return toReturn
 
 
-    def edit_user(self, user_ID, email, password, role, phoneNumber, address, firstName, lastName):
-        pass
-
+    def edit_user(self, user_ID, email, password, phoneNumber, address, firstName, lastName):
+        if self.role != "Supervisor":
+            return False
+        if user_ID is None or email is None or password is None or phoneNumber is None or address is None or firstName is None or lastName is None:
+            return False
+        if len(password) < 7 or len(phoneNumber) < 15:
+            return False
+        User.objects.filter(User_ID=user_ID, email=email, password=password, phoneNumber=phoneNumber, address=address,
+                            firstName=firstName, lastName=lastName).update()
+        toReturn = User.object.filter(User_ID=user_ID).exists()
+        return toReturn
 
     def account_role(self, user_ID):
-        pass
+        if not (User.objects.filter(User_ID=user_ID).exists()):
+            return False
+        if self.role != "TA":
+            return False
 
+        toReturn = User.object.filter(User_ID=user_ID).exists()
+        return toReturn
 
     def view_account(self, user_ID):
-        pass
+        if not (User.objects.filter(User_ID=user_ID).exists()):
+            return "INVALID"
+        user = User.objects.filter(User_ID=user_ID)
+        if self.role == "Supervisor":
+            return user.User_FName + ", " + user.User_LName + ": " + user.User_Role
+        if (self.role == "Instructor" and user.User_Role == "Supervisor") or (self.role == "TA" and user.User_Role == "Instrctor"):
+            return "INVALID"
+
+        return user.User_FName + ", " + user.User_LName + ": " + user.User_Role
