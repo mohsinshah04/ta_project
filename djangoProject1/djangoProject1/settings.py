6561a6e89d616c0e1758a393cac79d4ea6d94aea
emmet <ehbenck@uwm.edu>
2024-04-11 21:49:54 -0500

    Trying to get tests to work, still failing

--- djangoProject1/djangoProject1/settings.py
+++ djangoProject1/djangoProject1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ta_app'
+    'ta_app.apps.TaAppConfig'
 ]
 
 MIDDLEWARE = [
