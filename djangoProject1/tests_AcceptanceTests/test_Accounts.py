c03e0f32921008e961941e6bd7e62ade6af14fcb
emmet <ehbenck@uwm.edu>
2024-04-17 00:37:18 -0500

    Added more tests

--- djangoProject1/tests_AcceptanceTests/test_Accounts.py
+++ djangoProject1/tests_AcceptanceTests/test_Accounts.py
@@ -7,13 +7,29 @@ class AccountsTest(TestCase, Client):
         self.client = Client()
         self.SUuser_Role = Role(Role_Name="Supervisor")
         self.role = Role.objects.create(Role_Name="Supervisor")
+        self.test_role = Role(Role_Name='Instructor')
+        self.test_role_ta = Role(Role_Name='TA')
+        self.test_role.save()
+        self.test_role_ta.save()
         self.role.save()
         self.SUuser_Role.save()
         self.user = User.objects.create(User_Email="genericUser@uwm.edu", User_Password="<PASSWORD>", User_Role=self.role, User_Home_Address="123, Ridgeview Ct, Portage WI", User_FName="Generic", User_LName="User",
                                             User_Phone_Number="1+(608)654-2321")
         self.user.save()
-        self.test_user = User.objects.create(User_Role=self.SUuser_Role, User_Email= "Super@uwm.edu", User_FName = "John", User_LName = "Johnson",User_Phone_Number = "1+(608)542-2343",User_Home_Address = "123, Ridgeview Ct")
-        self.test_user.save()
+        self.test_user = User.objects.create(User_Role=self.SUuser_Role, User_Email= "Super@uwm.edu", User_FName = "John",
+                                             User_LName = "Johnson", User_Phone_Number = "1+(608)542-2343",
+                                             User_Home_Address = "123, Ridgeview Ct")
+
+        self.test_user_in = User.objects.create(User_Role=self.test_role, User_Email="Instrc@uwm.edu",
+                                                User_FName="Jose", User_LName="Johnson",
+                                                User_Phone_Number="1+(608)532-2343",
+                                                User_Home_Address="123, Ridgeview Ct")
+
+        self.test_user_ta = User.objects.create(User_Role=self.test_role_ta, User_Email="TA@uwm.edu",
+                                                User_FName="Joann",
+                                                User_LName="Johnson",
+                                                User_Phone_Number="1+(608)522-2343",
+                                                User_Home_Address="123, Ridgeview Ct")
         self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password}, follow=True)
 
     def test_view_account(self):
@@ -23,6 +39,22 @@ class AccountsTest(TestCase, Client):
     def test_user_doesnt_exist(self):
         response = self.client.post('/accounts/', {'id': 12323})
         self.assertEqual(response.context['message'], "Invalid account id: 12323")
+
+    def test_in_view_su(self):
+        self.client.post("/", {"Email": self.test_user_in.User_Email, "Password": self.test_user_in.User_Password}, follow=True)
+        response = self.client.post('/accounts/', {'id': self.test_user.id})
+        self.assertEqual(response.context['message'], "You cannot view this account because of your role")
+
+    def test_ta_view_su(self):
+        self.client.post("/", {"Email": self.test_user_ta.User_Email, "Password": self.test_user_ta.User_Password}, follow=True)
+        response = self.client.post('/accounts/', {'id': self.test_user.id})
+        self.assertEqual(response.context['message'], "You cannot view this account because of your role")
+
+    def test_ta_view_in(self):
+        self.client.post("/", {"Email": self.test_user_ta.User_Email, "Password": self.test_user_ta.User_Password}, follow=True)
+        response = self.client.post('/accounts/', {'id': self.test_user_in.id})
+        self.assertEqual(response.context['message'], "You cannot view this account because of your role")
+
 
 
 
