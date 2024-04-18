57c2863788894ef7a42728029e0a56afbc800a06
mohsinshah04 <shahmohsin4882@gmail.com>
2024-04-18 14:44:01 -0500

    fixed views.py messaging

--- djangoProject1/ta_app/views.py
+++ djangoProject1/ta_app/views.py
@@ -25,7 +25,7 @@ class LoginPage(View):
         if user_doesnt_exist:
             return render(request, "loginPage.html", {"message": "Email and password do not exists. Please contact your supervisor to get your account created"})
         elif bad_password:
-            return render(request, "loginPage.html", {"message": "bad password"})
+            return render(request, "loginPage.html", {"message": "Incorrect Password, please try again."})
         else:
             request.session["id"] = m.id
             return redirect("/home/")
