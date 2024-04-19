40853eaa379d023c2926fb3197ea4938edb0f232
youngc311 <112960473+youngc311@users.noreply.github.com>
2024-04-18 22:15:15 -0500

    Created and Implemented RoleClass methods

--- djangoProject1/classes/RoleClass.py
+++ djangoProject1/classes/RoleClass.py
@@ -7,27 +7,43 @@ class RoleClass:
 
     @classmethod
     def create_role(self, roleName, user):
-        if (roleName == None):
+        if roleName is None:
             return False
-        if (Role.objects.filter(name=roleName).exists() != False):
+        if type(user) != User:
             return False
-        if (user.User_Role.Role_Name != 'Supervisor'):
+        if Role.objects.filter(name=roleName).exists():
+            return False
+        if user.User_Role.Role_Name != 'Supervisor':
+            return False
+
+        validRole = ["TA", "Supervisor", "Instructor"]
+
+        validCheck = False
+        for term in validRole:
+            if term in roleName:
+                validCheck = True
+
+        if not validCheck:
             return False
 
         role = Role.objects.create(name=roleName)
 
-        if(role == None):
+        if role is None:
             return False
 
         return True
 
     @classmethod
     def delete_role(self, roleName, user):
-        if(roleName == None):
+        if roleName is None:
             return False
-        if(Role.objects.filter(name=roleName).exists() == False):
+        if type(user) != User:
             return False
-        if(user.User_Role.Role_Name != 'Supervisor'):
+        if not Role.objects.filter(name=roleName).exists():
+            return False
+        if not User.objects.filter(id=user.id).exists():
+            return False
+        if user.User_Role.Role_Name != 'Supervisor':
             return False
 
         Role.objects.filter(id=roleName).delete()
