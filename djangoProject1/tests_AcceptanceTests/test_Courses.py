90d91d207e502a8fa3edbaad62ae045852547d48
bleon-muqaj <bmuqaj@uwm.edu>
2024-04-20 01:09:34 -0500

    Create Courses Acceptance Tests

--- djangoProject1/tests_AcceptanceTests/test_Courses.py
+++ djangoProject1/tests_AcceptanceTests/test_Courses.py
@@ -85,3 +85,101 @@ class TestCoursesView(TestCase):
     def test_NotLoggedIn(self):
         response = self.client.get('/courses/', {"id": self.user.id}, follow=True)
         self.assertContains(response, "Please log in to view this page.")
+
+
+class TestCoursesCreate(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.semester = Semester.objects.create(Semester_Name="Fall 2024")
+        self.RoleTA = Role.objects.create(Role_Name="TA")
+        self.Role = Role.objects.create(Role_Name="Supervisor")
+        self.RoleProf = Role.objects.create(Role_Name="Instructor")
+        self.user = User.objects.create(User_FName="Supervisor", User_LName="User", User_Email="admin@uwm.edu", User_Password = "admin", User_Role = self.Role)
+
+        self.course = Course.objects.create(Course_Name="MATH - 201", Course_Description="Calculus", Course_Semester_ID_id = self.semester.id)
+        self.course2 = Course.objects.create(Course_Name="CS - 351", Course_Description="Data Structures and Algos", Course_Semester_ID_id = self.semester.id)
+        self.userTA = User.objects.create(User_FName="John", User_LName="Pork", User_Email="ta@uwm.edu", User_Password = "tapassword", User_Role = self.RoleTA)
+        self.userTA2 = User.objects.create(User_FName="Davis", User_LName="Clark", User_Email="ta2@uwm.edu", User_Password = "ta2password", User_Role = self.RoleTA)
+
+        Assign_User_Junction.objects.create(Course_ID=self.course, User_ID=self.userTA)
+        Assign_User_Junction.objects.create(Course_ID=self.course, User_ID=self.userTA2)
+        Assign_User_Junction.objects.create(Course_ID=self.course2, User_ID=self.userTA)
+
+        self.userProf = User.objects.create(User_FName="Himmithy", User_LName="Him", User_Email="prof@uwm.edu", User_Password="password", User_Role=self.RoleProf)
+        self.userProf1 = User.objects.create(User_FName="New", User_LName="Test", User_Email="prof@uwm.edu", User_Password = "prof", User_Role = self.RoleProf)
+
+        self.junctionUserProfToCourse = Assign_User_Junction.objects.create(User_ID=self.userProf, Course_ID = self.course2)
+
+        self.legalCode = "COMPSCI - 351"
+        self.legalCourseName = "Data Structures and Algorithims"
+        self.legalCourseDescription = "Blah blah blah"
+        self.legalSemesterYear = 2024
+        self.legalSemesterMonth = "Spring"
+
+        self.user.save()
+        self.userTA.save()
+        self.userTA2.save()
+        self.userProf.save()
+        self.userProf1.save()
+
+
+    def test_coursesCreateWithAssignedUsersExistingSemester(self):
+        self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password},
+                         follow=True)
+        response = self.client.post("/courseCreate/", {"courseCode": self.legalCode, "courseName": self.legalCourseName, "courseDescription": self.legalCourseDescription, "semester": self.semester.id})
+        self.assertEqual(response.url, "/courses/")
+
+    def test_coursesCreateWithAssignedUsersNewSemester(self):
+        self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password})
+        response = self.client.post("/courseCreate/", {"courseCode": self.legalCode, "courseName": self.legalCourseName,
+                                                       "courseDescription": self.legalCourseDescription,
+                                                       "semesterYear": self.legalSemesterYear, "semesterMonth": self.legalSemesterMonth, "semester": "new"})
+        self.assertEqual(response.url, "/courses/")
+
+    def test_coursesCreateWithAssignedInvalidUserNewSemester(self):
+        self.client.post("/", {"Email": self.userTA.User_Email, "Password": self.userTA.User_Password})
+        response = self.client.post("/courseCreate/", {"courseCode": self.legalCode, "courseName": self.legalCourseName,
+                                                       "courseDescription": self.legalCourseDescription,
+                                                       "semesterYear": self.legalSemesterYear,
+                                                       "semesterMonth": self.legalSemesterMonth, "semester": "new"})
+        self.assertEqual(response.context['message'], "INVALID")
+
+    def test_coursesCreateWithAssignedInvalidUserExistingSemester(self):
+        self.client.post("/", {"Email": self.userTA.User_Email, "Password": self.userTA.User_Password})
+        response = self.client.post("/courseCreate/", {"courseCode": self.legalCode, "courseName": self.legalCourseName,
+                                                       "courseDescription": self.legalCourseDescription,
+                                                       "semesterYear": self.legalSemesterYear,
+                                                       "semesterMonth": self.legalSemesterMonth, "semester": self.semester.id})
+        self.assertEqual(response.context['message'], "INVALID")
+
+    def test_coursesCreateWithAssignedUserExistingSemesterInvalidCode(self):
+        self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password})
+        response = self.client.post("/courseCreate/", {"courseCode": "33je9", "courseName": self.legalCourseName,
+                                                       "courseDescription": self.legalCourseDescription,
+                                                       "semesterYear": self.legalSemesterYear,
+                                                       "semesterMonth": self.legalSemesterMonth, "semester": self.semester.id})
+        self.assertEqual(response.context['error'], "Invalid Format: Try Again")
+
+    def test_coursesCreateWithAssignedUserExistingSemesterInvalidYear(self):
+        self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password})
+        response = self.client.post("/courseCreate/", {"courseCode": self.legalCode, "courseName": self.legalCourseName,
+                                                       "courseDescription": self.legalCourseDescription,
+                                                       "semesterYear": 2026,
+                                                       "semesterMonth": self.legalSemesterMonth, "semester": "new"})
+        self.assertEqual(response.context['error'], "Invalid Format: Try Again")
+
+    def test_coursesCreateWithAssignedUserExistingSemesterInvalidMonths(self):
+        self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password})
+        response = self.client.post("/courseCreate/", {"courseCode": self.legalCode, "courseName": self.legalCourseName,
+                                                       "courseDescription": self.legalCourseDescription,
+                                                       "semesterYear": 2022,
+                                                       "semesterMonth": "mmm", "semester": "new"})
+        self.assertEqual(response.context['error'], "Invalid Format: Try Again")
+
+    def test_coursesCreateWithAssignedUserExistingSemesterInvalidDescription(self):
+        self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password})
+        response = self.client.post("/courseCreate/", {"courseCode": self.legalCode, "courseName": self.legalCourseName,
+                                                       "courseDescription": "d",
+                                                       "semesterYear": 2022,
+                                                       "semesterMonth": "mmm", "semester": self.semester.id})
+        self.assertEqual(response.context['error'], "Invalid Format: Try Again")
