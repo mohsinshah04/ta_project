e9e9471e673f51068c86034218893a4d33e524ea
emmet <ehbenck@uwm.edu>
2024-04-23 22:25:23 -0500

    Last changes before the sprint is complete

---
+++ djangoProject1/tests_AcceptanceTests/test_Annoucment.py
@@ -0,0 +1,23 @@
+from django.test import TestCase, Client
+
+from ta_app.models import User, Role
+
+
+class AnnouncmentTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.role = Role(Role_Name="Supervisor")
+        self.role.save()
+        self.test_user = User.objects.create(User_Email="user@uwm.edu", User_Role=self.role, User_Password=
+                                             "<PASSWORD>", User_Phone_Number="3333333333", User_Home_Address="123 Milwaukee St",
+                                             User_FName="John", User_LName="User")
+        self.test_user.save()
+        self.client.post("/", {"Email": self.test_user.User_Email, "Password": self.test_user.User_Password}, follow=True)
+
+    def test_redirect_to_announcement(self):
+        response = self.client.post("/announcement/", {"User_List": self.test_user, "Message": "This is a test message"}, follow=True)
+        self.assertTrue(response.status_code == 200)
+
+    def test_page_not_found(self):
+        response = self.client.post("/announcement/", {"User_List": self.test_user, "Message": "This is a test message"}, follow=True)
+        self.assertFalse(response.status_code == 404)
