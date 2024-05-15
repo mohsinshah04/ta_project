37d4d03ce240cdf3368cb30d95d714847e5045b5
emmet <ehbenck@uwm.edu>
2024-05-15 12:18:40 -0500

    Finished putting logic from views into methods, and fixing acceptance tests for sections

--- djangoProject1/tests_AcceptanceTests/test_Sections.py
+++ djangoProject1/tests_AcceptanceTests/test_Sections.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from ta_app.models import Role, User, Course, Semester, Assign_User_Junction, Section
+from classes.SectionClass import SectionClass
 
 class TestSectionsView(TestCase):
     def setUp(self):
@@ -188,30 +189,86 @@ class TestSectionEdit(TestCase):
         self.RoleTA = Role.objects.create(Role_Name="TA")
         self.Role = Role.objects.create(Role_Name="Supervisor")
         self.RoleProf = Role.objects.create(Role_Name="Instructor")
-        self.user = User.objects.create(User_FName="Supervisor", User_LName="User", User_Email="admin@uwm.edu", User_Password = "admin", User_Role = self.Role)
+        self.user = User.objects.create(User_FName="Supervisor", User_LName="User", User_Email="admin@uwm.edu",
+                                        User_Password="admin", User_Role=self.Role)
 
-        self.course = Course.objects.create(Course_Name="MATH - 201", Course_Description="Calculus", Course_Semester_ID_id = self.semester.id)
-        self.course2 = Course.objects.create(Course_Name="CS - 351", Course_Description="Data Structures and Algos", Course_Semester_ID_id = self.semester.id)
-        self.userTA = User.objects.create(User_FName="John", User_LName="Pork", User_Email="ta@uwm.edu", User_Password = "tapassword", User_Role = self.RoleTA)
-        self.userTA2 = User.objects.create(User_FName="Davis", User_LName="Clark", User_Email="ta2@uwm.edu", User_Password = "ta2password", User_Role = self.RoleTA)
+        self.course = Course.objects.create(Course_Name="MATH - 201", Course_Description="Calculus",
+                                            Course_Semester_ID_id=self.semester.id)
+        self.course2 = Course.objects.create(Course_Name="CS - 351", Course_Description="Data Structures and Algos",
+                                             Course_Semester_ID_id=self.semester.id)
+        self.userTA = User.objects.create(User_FName="John", User_LName="Pork", User_Email="ta@uwm.edu",
+                                          User_Password="tapassword", User_Role=self.RoleTA)
+        self.userTA2 = User.objects.create(User_FName="Davis", User_LName="Clark", User_Email="ta2@uwm.edu",
+                                           User_Password="ta2password", User_Role=self.RoleTA)
 
         Assign_User_Junction.objects.create(Course_ID=self.course, User_ID=self.userTA)
         Assign_User_Junction.objects.create(Course_ID=self.course, User_ID=self.userTA2)
         Assign_User_Junction.objects.create(Course_ID=self.course2, User_ID=self.userTA)
 
-        self.userProf = User.objects.create(User_FName="Himmithy", User_LName="Him", User_Email="prof@uwm.edu", User_Password="password", User_Role=self.RoleProf)
-        self.userProf1 = User.objects.create(User_FName="New", User_LName="Test", User_Email="prof@uwm.edu", User_Password = "prof", User_Role = self.RoleProf)
+        self.userProf = User.objects.create(User_FName="Himmithy", User_LName="Him", User_Email="prof@uwm.edu",
+                                            User_Password="password", User_Role=self.RoleProf)
+        self.userProf1 = User.objects.create(User_FName="New", User_LName="Test", User_Email="prof@uwm.edu",
+                                             User_Password="prof", User_Role=self.RoleProf)
 
-        self.junctionUserProfToCourse = Assign_User_Junction.objects.create(User_ID=self.userProf, Course_ID = self.course2)
-
+        self.junctionUserProfToCourse = Assign_User_Junction.objects.create(User_ID=self.userProf,
+                                                                            Course_ID=self.course2)
+        form_data = {
+            'course': self.course.id,
+            'section_num': '801',
+            'section_type': 'Lab',
+            'meets_days': ['W'],
+            'campus': 'In-Person',
+            'start_date': '2023-09-01',
+            'end_date': '2023-12-15',
+            'credits': 3,
+            'start_time': '11:00',
+            'end_time': '1:20',
+            'building_name': 'Tech Building',
+            'room_number': '202',
+            'assigned_users[]': []
+        }
+        Section.objects.create(Course_ID=self.course, Section_Number=401, Section_Type="Lecture",
+                               Meets_Days=['T', "TR"],
+                               Start_Date="2023-09-01", End_Date="2023-12-15", Credits=3, Building_Name="Tech Building",
+                               Room_Number="202").save()
+        Section.objects.create(Course_ID=self.course, Section_Number=801, Section_Type="Lan",
+                               Meets_Days=['T'],
+                               Start_Date="2023-09-01", End_Date="2023-12-15", Credits=3, Building_Name="Tech Building",
+                               Room_Number="402").save()
+        Section.objects.create(Course_ID=self.course, Section_Number=802, Section_Type="Lan",
+                               Meets_Days=["TR"],
+                               Start_Date="2023-09-01", End_Date="2023-12-15", Credits=3, Building_Name="Tech Building",
+                               Room_Number="302").save()
         self.user.save()
         self.userTA.save()
         self.userTA2.save()
         self.userProf.save()
         self.userProf1.save()
-
-    def test_sectionsEditPageExists(self):
         self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password},
                          follow=True)
-        response = self.client.get('/sectionCreate/', {"id": self.user.id}, follow=True)
+
+    def test_sectionsEditPageExists(self):
+        section = Section.objects.get(Course_ID=self.course, Section_Number=401)
+        response = self.client.get('/sectionEdit/', {"course_id": self.course.id, "section_id":
+            section.id}, follow=True)
         self.assertEqual(response.status_code, 200)
+
+    def test_sectionsEditPageSuccessfulAssignUsersTA(self):
+        section = Section.objects.get(Course_ID=self.course, Section_Number=801)
+        form_data = {
+            "section_id": section.id,
+            "assigned_users": [self.userTA.id, self.userTA2.id]
+        }
+        response = self.client.post('/sectionEdit/', form_data)
+        self.assertTrue(Assign_User_Junction.objects.filter(User_ID=self.userTA2.id, Course_ID=self.course.id).exists())
+
+    def test_sectionsEditPageSuccessAssignUsersIN(self):
+        self.client.post("/", {"Email": self.user.User_Email, "Password": self.user.User_Password}, follow=True)
+        section = Section.objects.get(Course_ID=self.course, Section_Number=401)
+        form_data = {
+            "section_id": section.id,
+            "assigned_users": [self.userProf1.id]
+        }
+        response = self.client.post('/sectionEdit/', form_data)
+        self.assertTrue(
+            Assign_User_Junction.objects.filter(User_ID=self.userProf1.id, Course_ID=self.course.id).exists())
